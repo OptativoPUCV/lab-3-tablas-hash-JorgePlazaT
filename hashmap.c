25eb000423596104eefa7983442a12b8370fca0c
Your Name <you@example.com>
2025-04-19 15:30:49 -0400

    new try

--- hashmap.c
+++ hashmap.c
@@ -52,8 +52,12 @@ void enlarge(HashMap * map) {
 
 
 HashMap * createMap(long capacity) {
+    HashMap * map = (HashMap *) malloc(sizeof(HashMap));
+    map->capacity = capacity; 
+    map->size = 0;
+    map->current = -1;
 
-    return NULL;
+    return map;
 }
 
 void eraseMap(HashMap * map,  char * key) {    
