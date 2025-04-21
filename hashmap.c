aa7df22c9215edb63d789bc1617e634681a8b02f
Your Name <you@example.com>
2025-04-20 23:20:47 -0400

    new try

--- hashmap.c
+++ hashmap.c
@@ -60,14 +60,17 @@ void insertMap(HashMap * map, char * key, void * value) {
 
 void enlarge(HashMap * map) {
     enlarge_called = 1; //no borrar (testing purposes)
-    
-    Pair **auxPair = map->buckets;
-    int auxCapac = map->capacity;
-    map -> capacity *= 2;
-
+    Pair **oldBuckets = map->buckets;
+    int oldCapac = map->capacity;
+    map->capacity *= 2;
     map->buckets = (Pair**)calloc(map->capacity, sizeof(Pair*))
     map->size = 0;
 
+    for(int i = 0; i< map->oldCapac; i++){
+        if(oldBuckets[i] != NULL){
+            insertMap(map, oldBuckets[i]->key, oldBuckets[i]->value);
+        }
+    }
 }
 
 
